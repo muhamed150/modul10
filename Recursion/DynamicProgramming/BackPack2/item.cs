bcd663aef21fee741cd6629c2e525077c12d36b3
muhamed150 <63541574+muhamed150@users.noreply.github.com>
2020-09-13 12:47:45 +0300

    Create item.cs

---
+++ Recursion/DynamicProgramming/BackPack2/item.cs
@@ -0,0 +1,9 @@
+namespace Fibonaci
+{
+    public  class Item
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+    }
+}
